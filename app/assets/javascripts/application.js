076ac1e39c6cfcb1a70e8bde8b8093be8f8d0ea3
Maxym Wolberg <wolberg.maxym91@bloomfield.org>
2018-07-11 15:31:41 +0000

    added js file as well as more conditional work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,73 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+console.log("Hello World");
+/*
+$(document.ready(function(){
+    $("u1").on("click",input[type="checkbox"],function()
+        {$(this).closest("li").toggleClass("completed-todo") })
+}))
+*/
+
+
+//$(document).ready(console.log("My life is a lie"));
+    
+/*
+$(document).ready(function({ 
+    console.log("im ready");
+    $("#add-todo-btn").click(function(){
+     console.log("button functions are up to code")
+    })
+}
+)
+)
+*/
+$(document).ready(function(){
+    //$(".material-icons").hide()
+    console.log("Im ready")
+    $("#add-todo-btn").click(function(){
+        console.log("Buttons are now functional")
+        var nameOfTodo =$("#new-todo-name").val();
+        var timeOfTodo =$("#new-todo-days").val();
+        console.log(nameOfTodo)
+        console.log(timeOfTodo)
+
+        $("ul").append("<li> <input type='checkbox' class='check'><a href='show.html' style='text-decoration:none'> "+nameOfTodo+" </a><span class='day-est'>"+timeOfTodo+" day(s)</span></li>")
+            })
+            
+            $("ul").on("click","input[type='checkbox']",function(){
+        $(this).closest("li").toggleClass("completed-todo")
+        
+            })
+            
+         /*   
+            $("li").hover(function({$(this).closest(".material-icons").show()
+            console.log("checkmate");},
+                   function(){
+                    $(".material-icons").hide(););
+
+                    */
+                    /*
+            $("li").hover(function(){
+                console.log($(this).closest("i"))
+                $(this).closest(".material-icons").show()
+                console.log("checkmate");
+            })
+            */
+         /*  while( $(".bar").hover()) {
+                $(".material-icons").show();
+            }
+            */
+           setInterval(function(){
+                var $sample = $(".bar");
+                var $changed = $(".material-icons");
+             if($sample.is(":hover")) {
+                $changed.css("background", "red");
+             }
+                else {
+                $changed.css("background", "green");
+                    }
+            }, 200);
+            
+            
+})
