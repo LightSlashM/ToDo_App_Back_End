4255b29bf8ecac9ae194d91f0eea689c4e186fee
Maxym Wolberg <wolberg.maxym91@bloomfield.org>
2018-07-12 13:24:56 +0000

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'todo#index'
+  get'todo/index', to: 'todo#index'
   get'todo/new', to: 'todo#new'
   get'todo/create',to: 'todo#create'
   get 'todo/edit/:id', to: 'todo#edit'
